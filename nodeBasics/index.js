4759afc3dee2a415bf1c41e49049fda0ee2dfecb
“Vira <viraharasymiv78@gmail.com>
2026-02-18 01:00:45 +0200

    Trying primitive data types

--- nodeBasics/index.js
+++ nodeBasics/index.js
@@ -1,2 +1,17 @@
 import chalk from 'chalk';
-console.log(chalk.blue('Hello'));
+let age = null;
+let name = "vira";
+let isActive = true;
+let person = {
+    name: "vira",
+    age: 30,
+    isActive: true
+};
+const greet = function() {console.log("hello")};
+const numbers = [1, 2, 3, 4, 5];
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isActive);
+console.log(typeof person); 
+console.log(typeof greet);
+console.log(typeof numbers);
