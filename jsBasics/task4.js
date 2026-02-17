e03a8953bfbb9cd6cae21b45f0bf4ab676381fd3
“Vira <viraharasymiv78@gmail.com>
2026-02-18 01:35:13 +0200

    Task4

---
+++ jsBasics/task4.js
@@ -0,0 +1,16 @@
+// Task 4.1
+let radius = 5;
+let area = (Math.PI * radius ** 2).toFixed(2);
+console.log("Area of the circle: " + area);
+
+// Task 4.2
+let length = 10;
+let width = 5;
+let areaOfRectangle = length * width;
+console.log("Area of the rectangle: " + areaOfRectangle);
+
+// Task 4.3
+let radiusOfCylinder = 3;
+let heightOfCylinder = 7;
+let volumeOfCylinder = (Math.PI * radiusOfCylinder ** 2 * heightOfCylinder).toFixed(2);
+console.log("Volume of the cylinder: " + volumeOfCylinder);
